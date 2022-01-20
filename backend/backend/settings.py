127f9eb400e6184d05c489d83e4d55b837fe89d7
b1tTwiddler <bhattacharyasoumabrata@gmail.com>
2022-01-20 14:02:26 +0530

    corsheaders and rest_framework have been added in the installed_apps list.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo'
+    'todo',
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +101,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+# whitelisting React port
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000'
+)
 
 
 # Internationalization
